660d52e3ba7d937ab60251102c4109e899e09cba
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2024-12-23 22:47:46 -0300

    Adicionado arquivo Json

--- main.js
+++ main.js
@@ -2,118 +2,28 @@
 
 import { inicializarCarrinho } from "./src/menucarrinho";
 
-const catalogo = [
-  {
-    id: 1,
-    marca: "Zara",
-    nome: "Camisa Larga com Bolsos",
-    preco: 70,
-    imagem: "product-1.jpg",
-    feminino: false,
-  },
-  {
-    id: 2,
-    marca: "Zara",
-    nome: "Casaco Reto com Lã",
-    preco: 85,
-    imagem: "product-2.jpg",
-    feminino: true,
-  },
-  {
-    id: 3,
-    marca: "Zara",
-    nome: "Jaqueta com Efeito Camurça",
-    preco: 60,
-    imagem: "product-3.jpg",
-    feminino: false,
-  },
-  {
-    id: 4,
-    marca: "Zara",
-    nome: "Sobretudo em Mescla de Lã",
-    preco: 160,
-    imagem: "product-4.jpg",
-    feminino: false,
-  },
-  {
-    id: 5,
-    marca: "Zara",
-    nome: "Camisa Larga Acolchoada de Veludo Cotelê",
-    preco: 110,
-    imagem: "product-5.jpg",
-    feminino: false,
-  },
-  {
-    id: 6,
-    marca: "Zara",
-    nome: "Casaco de Lã com Botões",
-    preco: 170,
-    imagem: "product-6.jpg",
-    feminino: true,
-  },
-  {
-    id: 7,
-    marca: "Zara",
-    nome: "Casaco com Botões",
-    preco: 75,
-    imagem: "product-7.jpg",
-    feminino: true,
-  },
-  {
-    id: 8,
-    marca: "Zara",
-    nome: "Colete Comprido com Cinto",
-    preco: 88,
-    imagem: "product-8.jpg",
-    feminino: true,
-  },
-  {
-    id: 9,
-    marca: "Zara",
-    nome: "Blazer Azul De Algodão",
-    preco: 199,
-    imagem: "product-9.webp",
-    masculino: true,
-  },
-  {
-    id: 10,
-    marca: "Zara",
-    nome: "Jaqueta Beige",
-    preco: 125,
-    imagem: "product-10.webp",
-    masculino: true,
-  },
-  {
-    id: 11,
-    marca: "Zara",
-    nome: "Double-Faced",
-    preco: 115,
-    imagem: "product-11.jpeg",
-    feminino: true,
-  },
-  {
-    id: 12,
-    marca: "Zara",
-    nome: "Trench Coat Plus Size",
-    preco: 105,
-    imagem: "product-12.webp",
-    feminino: true,
-  },
-];
+async function carregarCatalogo() {
+  try {
+    const response = await fetch("./catalogo.json");
+    const catalogo = await response.json();
 
-for (const produtoCatalogo of catalogo) {
-  const cartaoProduto = `<div class="border-solid border-2 border-gray-600 w-72 m-2" id="card-produto-${produtoCatalogo.id}">
-          <img 
-            src="./assets/img/${produtoCatalogo.imagem}"
-            alt="Produto 1 do e-commerce Zara"
-          />
-          <p>${produtoCatalogo.marca}</p>
-          <p>${produtoCatalogo.nome}</p>
-          <p>$${produtoCatalogo.preco}</p>
-          <button>Adicionar</button>
-        </div>`;
-
-  document.getElementById("container-produto").innerHTML += cartaoProduto;
+    for (const produtoCatalogo of catalogo) {
+      const cartaoProduto = `<div class="border-solid border-2 border-gray-600 w-72 m-2" id="card-produto-${produtoCatalogo.id}">
+              <img 
+                src="./assets/img/${produtoCatalogo.imagem}"
+                alt="Produto 1 do e-commerce Zara"
+              />
+              <p>${produtoCatalogo.marca}</p>
+              <p>${produtoCatalogo.nome}</p>
+              <p>$${produtoCatalogo.preco}</p>
+              <button>Adicionar</button>
+            </div>`;
+      document.getElementById("container-produto").innerHTML += cartaoProduto;
+    }
+    inicializarCarrinho();
+  } catch (error) {
+    console.error("Ocorreu um erro ao carregar o catálogo:", error);
+  }
 }
 
-inicializarCarrinho();
+carregarCatalogo();
