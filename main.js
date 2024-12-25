69bc1204351c04c2e7a41ed3248a0ac7eab9364c
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2024-12-24 22:18:48 -0300

    Renderizando a página com os cards

--- main.js
+++ main.js
@@ -1,29 +1,7 @@
 "use.strict";
 
+import { renderizarCatalogo } from "./src/cartaoProduto";
 import { inicializarCarrinho } from "./src/menucarrinho";
 
-async function carregarCatalogo() {
-  try {
-    const response = await fetch("./catalogo.json");
-    const catalogo = await response.json();
-
-    for (const produtoCatalogo of catalogo) {
-      const cartaoProduto = `<div class="border-solid border-2 border-gray-600 w-72 m-2" id="card-produto-${produtoCatalogo.id}">
-              <img 
-                src="./assets/img/${produtoCatalogo.imagem}"
-                alt="Produto 1 do e-commerce Zara"
-              />
-              <p>${produtoCatalogo.marca}</p>
-              <p>${produtoCatalogo.nome}</p>
-              <p>$${produtoCatalogo.preco}</p>
-              <button>Adicionar</button>
-            </div>`;
-      document.getElementById("container-produto").innerHTML += cartaoProduto;
-    }
-    inicializarCarrinho();
-  } catch (error) {
-    console.error("Ocorreu um erro ao carregar o catálogo:", error);
-  }
-}
-
-carregarCatalogo();
+renderizarCatalogo();
+inicializarCarrinho();
