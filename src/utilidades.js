0aae270a6235d8ded171cf76c14ad18165e28413
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2024-12-28 16:22:45 -0300

    Colocando Javascript para adicionar ou retirar itens nos produtos

--- src/utilidades.js
+++ src/utilidades.js
@@ -1,6 +1,6 @@
 export const catalogo = [
   {
-    id: 1,
+    id: "1",
     marca: "Zara",
     nome: "Camisa Larga com Bolsos",
     preco: 70,
@@ -8,7 +8,7 @@ export const catalogo = [
     feminino: false,
   },
   {
-    id: 2,
+    id: "2",
     marca: "Zara",
     nome: "Casaco Reto com Lã",
     preco: 85,
@@ -16,7 +16,7 @@ export const catalogo = [
     feminino: true,
   },
   {
-    id: 3,
+    id: "3",
     marca: "Zara",
     nome: "Jaqueta com Efeito Camurça",
     preco: 60,
@@ -24,7 +24,7 @@ export const catalogo = [
     feminino: false,
   },
   {
-    id: 4,
+    id: "4",
     marca: "Zara",
     nome: "Sobretudo em Mescla de Lã",
     preco: 160,
@@ -32,7 +32,7 @@ export const catalogo = [
     feminino: false,
   },
   {
-    id: 5,
+    id: "5",
     marca: "Zara",
     nome: "Camisa Larga Acolchoada de Veludo Cotelê",
     preco: 110,
@@ -40,7 +40,7 @@ export const catalogo = [
     feminino: false,
   },
   {
-    id: 6,
+    id: "6",
     marca: "Zara",
     nome: "Casaco de Lã com Botões",
     preco: 170,
@@ -48,7 +48,7 @@ export const catalogo = [
     feminino: true,
   },
   {
-    id: 7,
+    id: "7",
     marca: "Zara",
     nome: "Casaco com Botões",
     preco: 75,
@@ -56,7 +56,7 @@ export const catalogo = [
     feminino: true,
   },
   {
-    id: 8,
+    id: "8",
     marca: "Zara",
     nome: "Colete Comprido com Cinto",
     preco: 88,
@@ -64,35 +64,35 @@ export const catalogo = [
     feminino: true,
   },
   {
-    id: 9,
+    id: "9",
     marca: "Zara",
     nome: "Blazer Azul De Algodão",
     preco: 199,
     imagem: "product-9.webp",
-    masculino: "true",
+    masculino: true,
   },
   {
-    id: 10,
+    id: "10",
     marca: "Zara",
     nome: "Jaqueta Beige",
-    preco: "125",
+    preco: 125,
     imagem: "product-10.webp",
-    masculino: "true",
+    masculino: true,
   },
   {
-    id: 11,
+    id: "11",
     marca: "Zara",
     nome: "Double-Faced",
-    preco: "115",
+    preco: 115,
     imagem: "product-11.jpeg",
-    feminino: "true",
+    feminino: true,
   },
   {
-    id: 12,
+    id: "12",
     marca: "Zara",
     nome: "Trench Coat Plus Size",
-    preco: "105",
+    preco: 105,
     imagem: "product-12.webp",
-    feminino: "true",
+    feminino: true,
   },
 ];
