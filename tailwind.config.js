541590546e47cc622b182037b8aaa1e9c95473b4
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-01-05 21:01:25 -0300

    Corregindo Formataçao tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,11 @@
 export default {
   content: ["./src/**/*.{html,js}", "./*.{html,js}"],
   theme: {
-    extend: {},
-  },
-  plugins: [],
-};
-
-module.exports = {
-  theme: {
     extend: {
       screens: {
         xs: "480px", // Define um novo breakpoint chamado `xs`
       },
     },
   },
+  plugins: [],
 };
